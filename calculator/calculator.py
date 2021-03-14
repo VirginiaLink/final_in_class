82c78b2d4563e76920e72fab28f8da4b6e1770de
VirginiaLink <linkv@oregonstate.edu>
2021-03-13 20:59:56 -0800

    second commit

---
+++ calculator/calculator.py
@@ -0,0 +1,12 @@
+# calculator app for last in class assignment
+# Virginia Link
+
+def sub(a,b):
+    if a < b:
+        print("a must be larger than b")
+        return 0
+    else:
+        return a-b
+
+def add(a, b):
+    return a+b
